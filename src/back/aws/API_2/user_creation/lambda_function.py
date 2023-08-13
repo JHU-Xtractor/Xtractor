642f9d5184ce2516babb148a173e07751989bb3c
jonat <jonathany330@gmail.com>
2023-08-12 22:34:07 -0400

    AWS SES updates

--- src/back/aws/API_2/user_creation/lambda_function.py
+++ src/back/aws/API_2/user_creation/lambda_function.py
@@ -4,24 +4,31 @@ import json
 import boto3
 dynamodb = boto3.resource('dynamodb',
                           region_name="us-east-1")
+s3 = boto3.client('s3')
+ses = boto3.client('ses')
+
 TABLENAME = 'xtractor_users'
 
 def lambda_handler(event,context):
+    
+    print(event)
+    bodyText = (event)
+
     dynamoDB = dynamodb.Table(TABLENAME)
     # for the response of the lambda function
     response = "Username Already Exists"
 
     # check if the table contains that user
-    if not checkIfUserExists(dynamoDB,json.loads(event['body'])['username']):
+    if not checkIfUserExists(dynamoDB,bodyText['username']):
         # parse the event for fields
-        bodyText = event['body']
         response = putIntoDynamoDB(dynamoDB, bodyText)
 
-        # create folder for s3``
-        if response == "User Successfully Inputted into Database\n":
-            response = response + createFolder(json.loads(bodyText)['username'])
+    #create folder for s3
+    if response == "User Successfully Inputted into Database\n":
+        response = response + createFolder(bodyText['username'])
             
     # return the lambda processes's response
+    print(response)
     return response
 
 def checkIfUserExists(dynamoDB,username):
@@ -41,23 +48,28 @@ def checkIfUserExists(dynamoDB,username):
 
 
 def putIntoDynamoDB(dynamoDB, bodyText):
-    dictionary = json.loads(bodyText)
     try:
+        # check if email is a edu account
+        if 'edu' not in bodyText['email'] :
+            response = "User attempted to put in a non edu account into the database."
+        else: 
         # putting the item
-        dynamoDB.put_item(
-            Item={
-                'username': dictionary['username'],
-                'fields':{
-                    'first_name': dictionary['name']['first_name'],
-                    'last_name': dictionary['name']['last_name'],
-                    'email': dictionary['email'],
-                    'security_question': dictionary['security']['security_question'],
-                    'security_answer': dictionary['security']['security_answer'],
-                    'email_verified': {'BOOL':False}
+            dynamoDB.put_item(
+                Item={
+                    'username': bodyText['username'],
+                    'fields':{
+                        'first_name': bodyText['name']['first_name'],
+                        'last_name': bodyText['name']['last_name'],
+                        'email': bodyText['email'],
+                        'security_question': bodyText['security']['security_question'],
+                        'security_answer': bodyText['security']['security_answer'],
+                        'email_verified': {'BOOL':False},
+                        'password':bodyText['security']['password']
+                        }
                     }
-                }
-        )
-        response = "User Successfully Inputted into Database\n"
+            )
+            verifyEmail(bodyText['email'])
+            response = "User Successfully Inputted into Database\n"
     except Exception as e:
         # in case something goes wrong
         response = str(e)
@@ -65,13 +77,20 @@ def putIntoDynamoDB(dynamoDB, bodyText):
     
 def createFolder(username):
     try:
-        s3 = boto3.client('s3')
-        s3.put_object(Bucket='xtractor-main',Body='./utilities/userFile.txt',Key=(username+'/userFile.txt'))
+        print("finished")
+        s3.put_object(Bucket='xtractor-main',Body='utilities/userFile.txt',Key=(username+'/userFile.txt'))
         return "Object Successfully Inserted into S3\n"
     except Exception as e:
+        print("insertion unsuccessful")
         print(e)
-    return "Object Not Inserted into S3\n"
+        return "Object Not Inserted into S3\n"
 
-if __name__ == "__main__":
-    response = createFolder('test')
-    print(response)
+def verifyEmail(email):
+    try:
+        response = ses.verify_email_identity(
+            EmailAddress=email
+        )
+        return response
+    except Exception as e:
+        print(e)
+        return e
