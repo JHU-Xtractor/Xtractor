027fda5b74fdc50cbcff47aa10c2d6c2a19bc5fa
jonat <jonathany330@gmail.com>
2023-10-23 14:11:03 -0400

    Sign up component

--- src/front/aws_frontend/src/amplify/xtractor-web/src/index.js
+++ src/front/aws_frontend/src/amplify/xtractor-web/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// Add needed imports
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+import ReactDOM from 'react-dom'
+import awsconfig from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AmplifyProvider } from '@aws-amplify/ui-react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+Amplify.configure(awsconfig);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// use the Amplify Provider component to intialize CSS
+ReactDOM.render(
+  <AmplifyProvider>
+<ThemeProvider theme={studioTheme}><App /></ThemeProvider>
+    </AmplifyProvider>,
+  document.getElementById('root')
+)
